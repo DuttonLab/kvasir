58a7d96e2a40731e13d68c4c4138a99c5f70f405
Kevin Bonham, PhD <kevbonham@gmail.com>
2015-04-09 23:13:05 -0400

    Added .gitignore so BLAST databased aren't added

---
+++ mongo_test2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# by Kevin Bonham, PhD (2015)
+# for Dutton Lab, Harvard Center for Systems Biology, Cambridge MA
+# CC-BY
+
+from __future__ import print_function
+from pymongo import MongoClient
+import subprocess
+
+def create_mongo_database(database_name):
+    subprocess.call("mongod --dbpath ~/computation/db", shell=True)
+    client = MongoClient()
+    db = client[database_name]
+    collection = db['test_collection']
+    collection.insert_one({'something':'something else'})
+    print('err... what\'s up?')
+    subprocess.call("mongod --shutdown")
+
+
+create_mongo_database('test4_database')
+    
+#db = MongoClient().demeter_test_db
+#
+#all_species = db.collection_names(False)
+#
+#print(all_species)
+#
+#for species in all_species:
+#    current_species_collection = db[species]
+#    print(current_species_collection)
+#    for gene in current_species_collection.find():
+#        print('>', gene['species'], ' | ', gene['locus_tag'], ' | ', gene['annotation'], '\n', gene['translation'], sep='')
+
+
+#print current_species
