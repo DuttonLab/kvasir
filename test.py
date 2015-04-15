d04455f5037b530797698b17b17de19226b26bda
Kevin Bonham, PhD <kevbonham@gmail.com>
2015-04-15 15:02:14 -0400

    Changed output_faa to fit BLAST naming convention

--- test.py
+++ test.py
@@ -3,10 +3,12 @@
 from pymongo import MongoClient
 from subprocess import Popen
 import os
+import atexit
 
 mongod = Popen(
         ["mongod", "--dbpath", '/Users/KBLaptop/computation/db/'],
     )
+print mongod.pid
 client = MongoClient()
 db = client['location_import_test']
 collection = db['test_collection']
@@ -16,7 +18,9 @@ all_species = db.collection_names(False)
 for species in all_species:
     current_species_collection = db[species]
     for gene in current_species_collection.find():
-        print gene['location']
+        print gene['locus_taag']
+        pass
 
-        
+
+      
 mongod.terminate()
