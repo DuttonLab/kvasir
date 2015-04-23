817450122e609638cae635c11ef49892f262f923
Kevin Bonham, PhD <kevbonham@gmail.com>
2015-04-23 13:29:59 -0400

    Get rid of lots of extraneous files for testing

--- mongo_deduplicate.py
+++ mongo_deduplicate.py
@@ -14,17 +14,18 @@ def dedupe(mongo_db_name):
     db = client[mongo_db_name]
     all_species = db.collection_names(False)
 
-    #for species in all_species:
-    current_species_collection = db['brachy_IMG']
+    for species in all_species:
+        current_species_collection = db[species]
 
-    for gene in current_species_collection.find():
-        current_tag, current_id = gene['locus_tag'], gene['_id']
-        current_tags = current_species_collection.find({'locus_tag':current_tag})
-        if current_tags.count() > 1:
-            for dupe in current_tags.distinct('_id')[1:]:
-                print 'removing:'
-                print dupe
-                current_species_collection.remove({'_id':dupe})
+        for gene in current_species_collection.find():
+            current_tag, current_id = gene['locus_tag'], gene['_id']
+            current_tags = current_species_collection.find({'locus_tag':current_tag})
+            if current_tags.count() > 1:
+                for dupe in current_tags.distinct('_id')[1:]:
+                    print 'removing:'
+                    print dupe
+                    current_species_collection.remove({'_id':dupe})
 
-# For testing
-dedupe('refactor_test')
+if __name__ == '__main__':
+    import sys
+    dedupe(sys.argv[1])
