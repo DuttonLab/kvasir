1d84aad04f9f4f553cce257713ee88413504ee9b
Kevin Bonham, PhD <kevbonham@gmail.com>
2015-04-23 18:48:06 -0400

    Change DataImport.py (a lot)
    - removed DataTypes.py
        - skipped gene and species objects (that's what the database is for)
    - added stuff to import DNA sequences and location on contig

--- DataImport.py
+++ DataImport.py
@@ -11,70 +11,56 @@ write it to a mongoDB database. Must have Mongod running, in terminal:
 
 import DataTypes as Data
 from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.Alphabet import IUPAC
 import re
-from subprocess import Popen
-import os
+from pymongo import MongoClient
 
-# Initial function to call. Opens and parses genbank file and creates Species objects
-# (see DataTypes.py) filled with Gene objects.
 def import_data(some_genbank, mongo_db_name):
+    client = MongoClient()
+    db = client[mongo_db_name]
+
     with open(some_genbank, 'r') as open_file:
-        current_species = Data.Species(get_species_name(some_genbank))
-        print 'Working on importing {0}'.format(current_species.species_name)
+        current_species = get_species_name(some_genbank)
+        species_collection = db[current_species]
+        print 'Working on importing {0}'.format(current_species)
 
         # Each "record" in genbank file is read, corresponds to individual contigs
         for record in SeqIO.parse(open_file, 'gb'):
-            current_species.add_contig(record.description)
-
-            # Within each record may be a number of CDS's annotated, this section
-            # pulls their descriptions and builds Gene objects        
+            current_contig = record.description
+       
             for feature in record.features:
                 if feature.type == 'CDS':
-                    cds = Data.Gene(
-                        feature.qualifiers['locus_tag'][0],
-                        feature.qualifiers['product'][0],
-                        feature.qualifiers['translation'][0],
-                        )
-                    cds.add_location(feature.location)
-                    print 'Adding {0} to import'.format(cds.annotation)
                     
-                    # Adds gene objects to Species object created above
-                    current_species.add_gene(cds)
-        
-        # Calls function (below) to build and populate MongoDB
-        write_database(current_species, mongo_db_name)
-            
+                    if feature.location.strand == 1:
+                        dna_seq = record.seq[
+                            feature.location.start:feature.location.end
+                        ]
+                    elif feature.location.strand == -1:
+                        dna_seq = record.seq[
+                            feature.location.start:feature.location.end
+                        ].reverse_complement()
+                    
+                    gene_record = {
+                        'species':current_species,
+                        'contig':current_contig,
+                        'location':str(feature.location),
+                        'locus_tag':feature.qualifiers['locus_tag'][0],
+                        'annotation':feature.qualifiers['product'][0],
+                        'dna_seq':str(dna_seq),
+                        'aa_seq':feature.qualifiers['translation'][0],
+                        }
+
+                    #species_collection.insert_one(gene_record)
+
+
 def get_species_name(path_to_genbank):
     name = re.search(r"(\w+)\.gb", path_to_genbank)
     return name.group(1)
 
-# Takes Species object built in import_data and adds it to MongoDB. Each database
-# may take multiple Species collections, which will be filled with individual gene
-# records.
-def write_database(species, mongo_db_name):
-    from pymongo import MongoClient
-    
-    # Connects to mongod server and connects to database specified in import_data.
-    # Collections are documents that contain individual species. This will append
-    # data if species collection already exists
-    client = MongoClient()
-    db = client[mongo_db_name]
-    species_collection = db[species.species_name]
-
-    for gene in species.genes:
-        gene_record = {
-            'species':species.species_name,
-            'locus_tag':gene.locus_tag,
-            'annotation':gene.annotation,
-            'translation':gene.aa_seq,
-        }
-        
-        print 'adding {0} to {1} database'.format(gene_record['annotation'], gene_record['species'])
-        species_collection.insert_one(gene_record)
-
 #For testing:
-#import_data('/Users/KBLaptop/computation/hgt/seqs/genomes/haloFixed.gb', 'location_import_test')
+import_data('/Users/KBLaptop/computation/genomes/arthro_piece.gb', 'prot-nucl')
 
-if __name__ == '__main__':
-    import sys
-    import_data(sys.argv[1], sys.argv[2])
+#if __name__ == '__main__':
+#    import sys
+#    import_data(sys.argv[1], sys.argv[2])
