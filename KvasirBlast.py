120e4db19161480764aeb73759fa1957fb60f60b
Kevin Bonham, PhD <kevbonham@gmail.com>
2015-06-15 14:23:05 -0400

    Working on blasting database

--- KvasirBlast.py
+++ KvasirBlast.py
@@ -6,8 +6,6 @@
 '''
 Must have Mongod running, in terminal: `mongod --dbpath path/to/db`
 '''
-
-from __future__ import print_function
 from pymongo import MongoClient
 from subprocess import Popen
 from Bio.Blast import NCBIXML
@@ -20,37 +18,41 @@ def kvasir_blast(mongo_db_name, blast_database):
     db = client[mongo_db_name]
 
     all_species = db.collection_names(False)
-    print(all_species)
+    print all_species
 
     for species in all_species:
         current_species_collection = db[species]
-        print(current_species_collection)
-        output_faa = './tmp/{0}.faa'.format(mongo_db_name)
+        print current_species_collection
+        output_faa = './tmp/{0}.fna'.format(mongo_db_name)
 
         for gene in current_species_collection.find():
             with open(output_faa, 'w+') as output_handle:
                 output_handle.write('{0}\n{1}\n'.format(
                     gene['locus_tag'],
-                    gene['translation'],
+                    gene['dna_seq'],
                     )
                 )
 
             blast_handle = NcbiblastnCommandline(
                 query=output_faa,
                 db=mongo_db_name,
-                pident=99,
+                perc_identity=99,
                 outfmt=5,
-                out="blast_out_tmp.xml",
+                out="/tmp/blast_out_tmp.xml",
                 max_hsps=20
                 )
-            print(blast_handle)
+            print blast_handle
             stdout, stderr = blast_handle()
 
-            blast_records = NCBIXML.parse('blast_out_tmp.xml')
-            print('hi there!', blast_records)
+            blast_records = NCBIXML.parse('./tmp/blast_out_tmp.xml')
+            print 'hi there!' 
+            print blast_records
             for blast_record in blast_records:
+                print blast_record
                 for alignment in blast_record.alignments:
+                    print alignment
                     for hsp in alignment.hsps:
+                        print hsp
                         if hsp.positives / alignment.length > 0.9:
                             print('****Alignment****')
                             print('sequence:', alignment.title)
@@ -63,40 +65,10 @@ def kvasir_blast(mongo_db_name, blast_database):
                             print('That one didn\'t match!')
 
             os.remove(output_faa)
-            os.remove('blast_out_tmp.xml')
+            os.remove('./tmp/blast_out_tmp.xml')
             
+#for testing
+kvasir_blast('pipe_test', 'pipe_test')
 
-
-
-def dont_use_this():
-    blast_handle = NcbiblastpCommandline(
-        query="/Users/KBLaptop/computation/hgt/seqs/genomes/brachy_IMG.faa",
-        db='mongo_test_again',
-        evalue=0.001,
-        outfmt=5,
-        out="test.xml"
-        )
-    print(blast_handle)
-    stdout, stderr = blast_handle()
-
-    result_handle = open("test.xml")
-
-    blast_records = NCBIXML.parse(result_handle)
-
-    for blast_record in blast_records:
-        print(blast_record.query)
-        print(blast_record.database)
-
-    for blast_record in blast_records:
-        for alignment in blast_record.alignments:
-            for hsp in alignment.hsps:
-                if hsp.positives / alignment.length > 0.9:
-                    print('****Alignment****')
-                    print('sequence:', alignment.title)
-                    print('length:', alignment.length)
-                    print('e value:', hsp.expect)
-                    print(hsp.query[0:75] + '...')
-                    print(hsp.match[0:75] + '...')
-                    print(hsp.sbjct[0:75] + '...')
 
 
--- testcmd.py
+++ testcmd.py
@@ -1,11 +1,32 @@
-import os
-import sys
+#import os
+#import sys
+#
+#print 'Here we go!'
+#gbk_folder = os.path.abspath(sys.argv[1])
+#
+#print 'Checking files...'
+#for the_file in os.listdir(gbk_folder):
+#    if not the_file.startswith('.'):
+#        if not os.path.isdir(the_file):
+#            print the_file
 
-print 'Here we go!'
-gbk_folder = os.path.abspath(sys.argv[1])
+def testing(mongo_db_name, blast_database):
+    from pymongo import MongoClient
 
-print 'Checking files...'
-for the_file in os.listdir(gbk_folder):
-    if not the_file.startswith('.'):
-        if not os.path.isdir(the_file):
-            print the_file
+    client = MongoClient()
+    db = client[mongo_db_name]
+
+    all_species = db.collection_names(False)
+    print all_species
+
+    for species in all_species:
+        current_species_collection = db[species]
+        print current_species_collection
+        output_faa = './tmp/{0}.faa'.format(mongo_db_name)
+
+        for gene in current_species_collection.find():
+            print gene['locus_tag']
+            print gene
+            
+
+testing('pipe_test', 'pipe_test')
