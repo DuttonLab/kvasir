b86ae3cfc04c9029b95c6185408dbb77f5c70ddc
Kevin Bonham, PhD <kevbonham@gmail.com>
2015-12-20 21:17:13 -0500

    Stop running mongo within script

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import user_settings
 import os
 import sys
-from subprocess import Popen
+
 
 from DataImport.gb_parse import parse_genbank
 
@@ -10,8 +10,5 @@ out_dir = user_settings.OUTPUT
 
 in_file = os.path.join(in_dir, sys.argv[1])
 
-mongod = Popen(['mongod', '--dbpath', user_settings.MONGOPATH])
-
 mongo_import(parse_genbank(in_file), 'test_collection')
 
-Popen.kill(mongod)
