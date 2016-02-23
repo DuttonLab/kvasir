45ed1d0a7c42b59bbd2381ca4e1864478ea3986b
Kevin Bonham, PhD <kevbonham@gmail.com>
2016-02-23 12:49:20 -0500

    finish minimal circos outputs - generate conf and karyotype files - generate GC plot files and include in main conf

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from settings import *
 from DataImport.mongo_import import mongo_import_genbank
 from FindHGT.make_blast_db import make_blast_db, db_cds_to_fna
 from FindHGT.run_blast import blast_all, parse_blast_results_xml
-from Analysis import output
+from Analysis import output, circos
 import os
 
 
@@ -37,4 +37,6 @@ def analyze():
 def run_circos():
     # circos.get_karyotypes()
     # circos.get_links(0.99, 400)
-    circos.get_conf_file("/Users/KBLaptop/computation/kv_data/output/database_name/circos/links/99-400-links.txt")
+    # circos.get_conf_file("/Users/KBLaptop/computation/kv_data/output/database_name/circos/links/99-400-links.txt",
+    #                      gc="/Users/KBLaptop/computation/kv_data/output/database_name/circos/GC/gc_plots.conf")
+    circos.get_gc_conf()
--- src/Analysis/circos.py
+++ src/Analysis/circos.py
@@ -4,10 +4,12 @@ import numpy as np
 from settings import OUTPUT, CIRCOS
 from settings import MONGODB as db
 from Analysis.output import get_hgt
-from subprocess import Popen
+from Bio import SeqUtils
+
 
 def safe_species_name(species_name):
     return re.sub('\W', '_', species_name)
+
 
 def get_karyotypes(length_cutoff=1000):
     """
@@ -106,8 +108,80 @@ thickness     = 1
 """.format(links))
 
         if gc:
-            out_handle.write("<<include {}>>".format(gc))
+            out_handle.write("<<include {}>>\n".format(gc))
 
         out_handle.write("<image>\n<<include {}>>\n</image>\n".format(os.path.join(CIRCOS, 'current', 'etc', 'image.conf')))
         out_handle.write("<<include {}>>\n".format(os.path.join(CIRCOS, 'current', 'etc', 'colors_fonts_patterns.conf')))
         out_handle.write("<<include {}>>\n".format(os.path.join(CIRCOS, 'current', 'etc', 'housekeeping.conf')))
+
+
+def get_gc():
+    for contig in db['genes'].find({'type': 'contig'}):
+        gc_points = []
+        seq = contig['dna_seq']
+
+        for p in range(len(seq))[500::1000]:
+            gc_cont = SeqUtils.GC(seq[p-500:p+499])
+            gc_points.append((p-500, p+499, gc_cont))
+        if gc_points:
+            yield safe_species_name(contig['species']), contig['contig_id'], gc_points
+
+
+def get_gc_conf(outfile = None):
+    outdir = os.path.join(OUTPUT, 'circos', 'GC')
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+    if not outfile:
+        outfile = os.path.join(outdir, 'gc_plots.conf')
+
+    with open(outfile, 'w+') as out_handle:
+        out_handle.write("<plots>\n\n")
+        for species, contig, gc_points in get_gc():
+            points_file = get_gc_points_file(species, contig, gc_points)
+            gc_values = [x[2] for x in gc_points]
+            stats = (min(gc_values), max(gc_values), np.average(gc_values), np.std(gc_values))
+            out_handle.write("""    <plot>
+    show  = yes
+    type  = line
+
+    file  = {}
+    r1    = 0.95r
+    r0    = 1.05r
+    max   = {}
+    min   = {}
+
+    color     = grey
+    thickness = 1
+
+    <rules>
+    <rule>
+    condition    = var(value) > {}
+    color = dgreen
+    </rule>
+    <rule>
+    condition    = var(value) < {}
+    color = dred
+    </rule>
+    </rules>
+    </plot>\n
+    """.format(points_file, stats[1], stats[0], stats[2]+stats[3], stats[2]-stats[3])
+                )
+        out_handle.write('</plots>\n')
+
+
+def get_gc_points_file(species, contig, gc_points):
+    outdir = os.path.join(OUTPUT, 'circos', 'GC')
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir)
+    contig_name = "{}-{}".format(species, contig)
+
+    with open(os.path.join(OUTPUT, 'circos', 'GC', "{}_gc.text".format(contig_name)), 'w+') as out_handle:
+        for point in gc_points:
+            out_handle.write("{} {} {} {}\n".format(
+                contig_name,
+                point[0],  # start
+                point[1],  # end
+                point[2],  # % GC
+            ))
+
+        return out_handle.name
