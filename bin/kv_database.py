fd66fc91780956c3e1282f0c5b0bbdb03cae6526
KEVIN BONHAM <kevbonham@gmail.com>
2017-05-29 14:11:32 -0400

    begin database operations

---
+++ bin/kv_database.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+import pymongo
+import argparse
+import os
+import logging
+from kvasir import database
+
+parser = argparse.ArgumentParser(description='Import genbank files')
+
+parser.add_argument("mongodb", help="The name of MongoDB database")
+parser.add_argument("command", help="Which database command to run",
+    choices=["dedupe", "delete", "list_species"])
+
+parser.add_argument("-s", "--species", help="Species on which to perform operations", nargs="+")
+
+parser.add_argument("-c", "--collection", help="Which collection to perform operations on",
+    choices=["genes", "blast_results", "species_distance"], default="genes")
+
+parser.add_argument("-v", "--verbose", help="Display info status messages", action="store_true")
+parser.add_argument("-q", "--quiet", help="Suppress most output", action="store_true")
+parser.add_argument("--debug", help="Set logging to debug", action="store_true")
+
+parser.add_argument("-l", "--log",
+    help="File path for log file")
+
+args = parser.parse_args()
+
+logpath = None
+if args.log:
+    logpath = os.path.abspath(args.log)
+    if os.path.isdir(logpath):
+        logpath = os.path.join(logpath, "kvasir.log")
+
+if args.debug:
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s || %(levelname)s: %(message)s", filename=logpath)
+elif args.verbose:
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s || %(levelname)s: %(message)s", filename=logpath)
+elif args.quiet:
+    logging.basicConfig(level=logging.ERROR, format="%(asctime)s || %(levelname)s: %(message)s", filename=logpath)
+else:
+    logging.basicConfig(level=logging.WARNING, format="%(asctime)s || %(levelname)s: %(message)s", filename=logpath)
+
+if args.command == "list_species":
+    database.list_species(args.mongodb, args.collection, args.species)
+elif args.command == "delete":
+    database.delete_species(args.mongodb, args.collection, args.species)
---
+++ kvasir/database.py
@@ -0,0 +1,36 @@
+import logging
+
+
+def delete_species(db, collection, species):
+    if collection == "genes":
+        logging.info("Deleting all gene records for {}".format(species))
+        db[collection].delete_many({"species":{"$in":species}})
+    elif collection == "blast_results":
+        logging.info("Deleting all blast results for {}".format(species))
+        db[collection].delete_many({"$or":[
+                        {"query_species":{"$in":species}},
+                        {"subject_species":{"$in":species}}]})
+    elif collection == "species_distance":
+        logging.info("Deleting all distance records for {}".format(species))
+        db[collection].delete_many({"$or":[
+                        {"species_1":{"$in":species}},
+                        {"species_2":{"$in":species}}]})
+    elif collection == "all":
+        logging.info("Deleting records of all types for {}".format(species))
+        db["genes"].delete_many({"species":{"$in":species}})
+        db["blast_results"].delete_many({"$or":[
+                        {"query_species":{"$in":species}},
+                        {"subject_species":{"$in":species}}]})
+        db["species_distance"].delete_many({"$or":[
+                        {"species_1":{"$in":species}},
+                        {"species_2":{"$in":species}}]})
+    else:
+        logging.error("{} is not a valid collection, no action taken".format(collection))
+
+
+def list_species(db, collection, species=[]):
+    species_list = db[collection].distinct("species")
+    if species:
+        species_list = set(species).intersect(set(species_list))
+    print("The {} database contains:".format(db))
+    print("\n".join(sorted(species_list)))
