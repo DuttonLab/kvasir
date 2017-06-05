786da7dcbcd460da0eef4a55ad8a3d39788f04b3
KEVIN BONHAM <kevbonham@gmail.com>
2017-06-05 13:17:59 -0400

    add travis config, test of mongodb

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -1,1 +1,9 @@
-# TODO: Make tests
+import pytest
+import pymongo
+
+def test_mongo():
+    db = pymongo.MongoClient()["db"]
+    db["collection"].insert_one({"key1":"value1", "key2":"value2"})
+
+    record = db["collection"].find_one({"key1":"value1"})
+    assert record["key2"] == "value2"
