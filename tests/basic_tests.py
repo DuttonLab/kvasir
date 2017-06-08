25c1675d322791bc4e54d777af86ffe67314110c
KEVIN BONHAM <kevbonham@gmail.com>
2017-06-08 13:47:08 -0400

    make tests for blastdb

--- tests/basic_tests.py
+++ tests/basic_tests.py
@@ -26,3 +26,40 @@ def test_genbank_import():
     assert record["annotation"] == "hypothetical protein"
     assert record["aa_seq"] == "MLKEQDRDLESRVWDAQRIYDKAERSLSWAQSTQQSRGVFKKLLHGASDQTKVDELTWELEQAQQVYEDISQQHDALAAELEAQKDTVERLRERDGELEDRMGFLDSMLFLTENGTSPFAEDQPDSFSSSADSYLGYDDSYDMSSSPVFGLSDDSSDGMEL"
     assert record["dna_seq"].lower() == "atgctaaaagagcaagaccgagacctagaatctcgcgtgtgggatgcgcaacgcatctacgacaaagcggagcgcagcctttcgtgggcgcagtcgacgcagcaatcgcgcggggtgtttaagaaacttctccacggagcatctgatcagacaaaggttgatgagcttacctgggaactcgagcaagcgcagcaggtgtatgaagatataagccagcagcatgacgcacttgcagctgaactcgaggcacagaaggacaccgtggagagactgcgtgagcgcgatggcgagctggaagacagaatgggcttcttggattccatgctgtttttgacagaaaacgggacgagtccatttgctgaagatcaaccagattccttttcctcatcagctgatagctacctcggatatgacgatagctatgacatgagtagctctcctgtttttgggcttagcgatgattcatctgatggaatggaactctaa"
+
+def test_blast_db_creation():
+    from kvasir import make_blast_db
+    import os
+
+    db = pymongo.MongoClient()["test_db"]
+    db["genes"].drop()
+
+    db["genes"].insert({
+        "type":"CDS",
+        "dna_seq":"aaatttggggcccc",
+        })
+    db["genes"].insert(
+        {
+        "type":"16s",
+        "dna_seq":"acgtactgatgcagct"},
+        {
+        "type":"16s",
+        "dna_seq":"aaatttggggcccc"
+        })
+
+    f1 = make_blast_db.db_to_fna(db, "genes")
+    lns = f1.readlines()
+    f1.seek(0)
+    assert lns[1] == "aaatttggggcccc\n"
+
+
+    f2 = make_blast_db.db_to_fna(db, "genes", "16s")
+    make_blast_db.make_blast_db(f2.name, "nucl", "./tmp")
+
+    assert os.path.isfile("./tmp.nhr")
+    assert os.path.isfile("./tmp.nin")
+    assert os.path.isfile("./tmp.nsq")
+
+    os.remove("./tmp.nhr")
+    os.remove("./tmp.nin")
+    os.remove("./tmp.nsq")
