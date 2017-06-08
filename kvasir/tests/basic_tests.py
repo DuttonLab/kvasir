35fe21d2b15ff10932a4bdccf803f9c855034687
KEVIN BONHAM <kevbonham@gmail.com>
2017-06-08 16:20:56 -0400

    analysis tests, reorganize test folder

--- kvasir/tests/basic_tests.py
+++ kvasir/tests/basic_tests.py
@@ -20,7 +20,7 @@ def test_genbank_import():
     db = pymongo.MongoClient()["test_db"]
     db["genes"].drop()
 
-    mongo_import.mongo_import_genbank("tests/testdata/jb4_partial.gb", db, "genes")
+    mongo_import.mongo_import_genbank("kvtests/testdata/jb4_partial.gb", db, "genes")
 
     record = db["genes"].find_one({"locus_tag":"Ga0099666_132","type":"CDS"})
     assert record["annotation"] == "hypothetical protein"
@@ -69,8 +69,8 @@ def test_blast_run():
     db = pymongo.MongoClient()["test_db"]
     db["genes"].drop()
 
-    mongo_import.mongo_import_genbank("tests/testdata/jb4_partial.gb", db, "genes")
-    mongo_import.mongo_import_genbank("tests/testdata/jb182_partial.gbk", db, "genes")
+    mongo_import.mongo_import_genbank("kvtests/testdata/jb4_partial.gb", db, "genes")
+    mongo_import.mongo_import_genbank("kvtests/testdata/jb182_partial.gbk", db, "genes")
 
     f1 = make_blast_db.db_to_fna(db, "genes")
     make_blast_db.make_blast_db(f1.name, "nucl", "./tmp")
@@ -86,7 +86,7 @@ def test_blast_run():
 
 
     blast_results = run_blast.blast_all(fasta, "tmp", perc_identity=0.99)
-    blast_results.seek(0) == 0
+    blast_results.seek(0)
     assert blast_results.readline() == '<?xml version="1.0"?>\n'
     blast_results.seek(0)
     assert 2080 >= len(list(blast_results)) > 2075
@@ -105,3 +105,21 @@ def test_blast_run():
     os.remove("./tmp.nhr")
     os.remove("./tmp.nin")
     os.remove("./tmp.nsq")
+
+def test_analysis():
+    from kvtests.blast_results import blast_results
+    from kvasir.output import hgt_groups, output_groups
+    from bson.objectid import ObjectId
+
+
+    db = pymongo.MongoClient()["test_db"]
+    db["blast_results"].drop()
+
+    db["blast_results"].insert_many(blast_results)
+
+    groups = hgt_groups(0.99, db)
+    assert len(groups) == 1
+    assert len(groups[0]) == 25
+
+    s = sorted(groups[0])
+    assert s[0] == ObjectId('5939a639009470375af931c3')
